b930af67de3a520fc1008e12b66e0737fad98735
Chris Shreve <c.k.shreve@gmail.com>
2012-05-30 10:08:42 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sabertooth::Application.config.secret_token = '827f65c99371de37b6765a06fa362a84fb70004ff7431931f7b503bd7734f22cb8b25c57b518cf28c9958a726a05c100cefd8f7c4607a2d1327455cd75579063'
