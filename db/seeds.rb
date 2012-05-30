6e266c60badfe106725293abcd3911f2a2e58e7e
Chris Shreve <c.k.shreve@gmail.com>
2012-05-30 16:45:51 -0700

    Seed with admn, mgmt and tech users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+
+user = User.create! :name => 'Joe Blo', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user2.name
-user.add_role :admin
+
+admn = User.create! :name => 'Kenny Blankenship', :email => 'admn@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << admn.name
+admn.add_role :admin
+
+mgmt = User.create! :name => 'Craig Smith', :email => 'mgmt@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New mgmt created: ' << mgmt.name
+mgmt.add_role :mgmt
+
+tech = User.create! :name => 'Doug Wilson', :email => 'tech@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New tech created: ' << tech.name
+tech.add_role :tech
