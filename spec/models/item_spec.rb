d06219e8cc9c58703089004ada416dd322ed1ff3
Chris Shreve <c.k.shreve@gmail.com>
2012-06-08 11:02:43 -0700

    Fixed tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe Item do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should do something" do
+    names = %w{ grey red blue green yellow orange }
+    names.map.with_index do |name, i|
+      puts "#{name} has rank #{i}"
+    end
+  end
+
+
+  it "should do act like a tree" do
+    puts "howdy"
+  end
+
 end
